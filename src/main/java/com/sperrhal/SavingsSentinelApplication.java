508e8fc4595875309aecf7291b87ee9605a0c7d4
dsglasse <dsglasse@ncsu.edu>
2015-12-07 13:09:44 -0500

    Initial commit

---
+++ src/main/java/com/sperrhal/SavingsSentinelApplication.java
@@ -0,0 +1,12 @@
+package com.sperrhal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingsSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavingsSentinelApplication.class, args);
+    }
+}
